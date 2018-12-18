228c470d9bb02c08c585d676bf7f385d38d0ee17
build <buildautomation@syncfusion.com>
2018-12-18 19:05:17 +0530

    Removed Old Samples

--- src/about/ej-bridge-viewmodel.js
+++
@@ -1,7 +0,0 @@
-import '../samples/scripts/jsondata.js';
-export class ViewModel {
-  constructor() {
-    this.EmployeesList = ej.DataManager(window.employeeView).executeLocal(ej.Query().take(8)); // eslint-disable-line new-cap
-    this.page = { pageSize: 4};
-  }
-}
--- src/samples/rotator/thumbnail.js
+++
@@ -1,7 +0,0 @@
-export class BasicUse {
-    constructor() {
-      this.slideWidth = '100%';
-      this.frameSpace = '0px';
-      this.slideHeight = 'auto';
-      this.thumbElement = 'thumbElement';
-    }}

5cc9d635c418abcdf901ccacd734f94d4e1eba9b
SyncfusionInstall <buildautomation@syncfusion.com>
2020-11-05 19:24:26 +0530

    v18.3.0.47

--- dist/src/samples/datetimepicker/RTL.js
+++
@@ -1,5 +0,0 @@
-export class RTL {
-    constructor() {
-      this.datetimeValue = '5/18/2018 5:00 AM';
-    }
-}
--- dist/src/samples/kanban/card-template.js
+++
@@ -1,9 +0,0 @@
-import './KanbanJsonData';
-export class cardTemplate {
-  constructor() {
-    this.tempKanbanData = tempKanbanData;
-    this.Field = { primaryKey: 'Id', content: 'Summary', tag: 'Tags', color: 'Type', imageUrl: 'ImgUrl', priority: 'RankId'};
-    this.CardSettings = { template: '#cardtemplate', colorMapping: {'#cb2027': 'Bug,Story', '#67ab47': 'Improvement', '#fbae19': 'Epic', '#6a5da8': 'Others'}};
-    this.ToolTip = {enable: true};
-  }
- }

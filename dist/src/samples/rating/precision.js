5ba3112948cb6bd98620a6c943503cba025f252e
buildautomation <buildautomation@syncfusion.com>
2020-12-17 17:50:47 +0530

    v18.4.0.30

--- dist/src/samples/rating/precision.js
+++
@@ -1,9 +0,0 @@
-export class PrecisionRating {
-  constructor() {
-    this.fullRatingValue = 4;
-    this.halfRatingValue = 3.5;
-    this.exactRatingValue = 3.7;
-    this.half = ej.Rating.Precision.Half;
-    this.exact = ej.Rating.Precision.Exact;
-  }
-}

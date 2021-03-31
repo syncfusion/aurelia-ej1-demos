cefa2b406fe868d6e9325d7f16df3a73719be888
buildautomation <buildautomation@syncfusion.com>
2021-03-31 21:13:32 +0530

    v19.1.0.54

--- src/samples/grid/column-Template.js
+++
@@ -1,8 +0,0 @@
-﻿import '../scripts/jsondata.js';
-export class columnTemplate {
-    constructor() {
-      this.EmployeesList = ej.DataManager(window.employeeView).executeLocal(ej.Query().take(8)); // eslint-disable-line new-cap
-      this.page = { pageSize: 4 };
-    }
-}
-

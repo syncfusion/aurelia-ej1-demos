5fb5c4cbc114c822dc812a8305668ae0f8a59935
buildautomation <buildautomation@syncfusion.com>
2021-06-30 12:32:25 +0530

    v19.2.0.44

--- dist/src/samples/daterangepicker/DateTimeRange.js
+++
@@ -1,6 +0,0 @@
-export class Meridian {
-    constructor() {
-      this.daterangeValue = '5/18/2018-5/29/2018';
-      this.enableTimePicker = true;
-    }
-}
--- src/samples/kanban/swimlane.js
+++
@@ -1,7 +0,0 @@
-import './KanbanJsonData';
-export class Swimlane {
-  constructor() {
-    this.swimlaneKanbanData = swimlaneKanbanData;
-    this.Field = { primaryKey: 'Id', content: 'Summary', imageUrl: 'ImgUrl', swimlaneKey: 'Assignee'};
-  }
-}

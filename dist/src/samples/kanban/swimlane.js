6f8a8b9200b280de00b3bca95c88faa4130bef52
BuildAutomation <buildautomation@syncfusion.com>
2021-09-30 15:14:33 +0530

    v19.3.0.43

---
+++ dist/src/samples/kanban/swimlane.js
@@ -0,0 +1,7 @@
+import './KanbanJsonData';
+export class Swimlane {
+  constructor() {
+    this.swimlaneKanbanData = swimlaneKanbanData;
+    this.Field = { primaryKey: 'Id', content: 'Summary', imageUrl: 'ImgUrl', swimlaneKey: 'Assignee'};
+  }
+}
